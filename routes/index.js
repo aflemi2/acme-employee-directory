a45e2b06f5aac4fc8990a8f9888dd45e458aee44
Alan Fleming <alalfleming@gmail.com>
2018-02-11 16:13:33 -0500

    working on post

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 const app = require('express').Router();
+const db = require('../db');
+const models = db.models;
+const { Employee } = models;
 
+module.exports= app;
 
 app.get('/', (req, res, next)=>{
-  res.send('yay');
+  res.render('index', { title: 'Home' });
 })
 
+app.get('/employees', (req, res, next)=>{
+  Employee.findAll()
+  .then(employees => res.render('employees', { title: 'Employees', employees }))
+  .catch(next);
+})
+
+app.get('/employees/:id', (req, res, next)=>{
+  Employee.findById(req.params.id)
+  .then( employee=> res.render('employee', { title: employee.fullName, employee }))
+  .catch(next);
+})
--- server.js
+++ server.js
@@ -1,15 +1,35 @@
+const db = require('./db');
+const path = require('path');
 const express = require('express');
 const app = express();
-const router = require('./routes');
+const nunjucks = require('nunjucks');
+nunjucks.configure({ noCache: true });
 
-const db = require('./db');
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
+
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
 const { Employee } = db.models;
+
 db.sync()
   .then(()=> db.seed());
 
-//app.use('/', router);
+  app.use((req, res, next)=>{
+  Employee.findAll()
+  .then( employees=> {
+    res.locals.employeeCount = employees.length;
+    next()
+  })
+  .catch(next)
+})
 
-app.get('/', ( req, res, next)=>{
+
+app.use('/', require('./routes'));
+
+
+app.get('/db', ( req, res, next)=>{
   Employee.findAll()
     .then( employees => res.send(employees))
     .catch(next);
